4d124f5a14979c45db8e2fb0f011c72641bf84ff
Tyler <tylerthomasallen@gmail.com>
2018-10-22 21:33:54 -0700

    Add user auth and validations

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const express = require("express")
-const app = express()
-const mongoose = require('mongoose')
-const db = require('./config/keys').mongoURI
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const passport = require('passport');
+require('./config/passport')(passport);
+
+
 mongoose
-  .connect(db)
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"))
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.get('/test', (req, res) => {
+  res.send('Hello, test!');
+});
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
