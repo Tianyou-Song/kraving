d4e55a8633b975670e0bb770a153f9d436f16b14
Tyler <tylerthomasallen@gmail.com>
2018-10-23 18:45:54 -0700

    Finish demo css for search dropdown

--- frontend/src/components/searchbar/searchbar_item.jsx
+++ frontend/src/components/searchbar/searchbar_item.jsx
@@ -4,12 +4,19 @@ class SearchBarItem extends React.Component {
 
   render() {
     const { bus } = this.props;
-    debugger;
 
     return (
       <div className="search-dropdown-item">
-        <img src={`/images/stars/${bus.rating}.png`}></img>
-        <li>{bus.name}</li>
+        <div>
+          <img className="search-dropdown-image" src={bus.image_url} />
+        </div>
+        <div className="search-dropdown-text">
+          <li className="search-dropdown-title">{bus.name}</li>
+          <div className="search-dropdown-stars">
+            <img className="search-stars"src={`/images/stars/${bus.rating}.png`}></img>
+            <li className="search-review-count">({bus.review_count})</li>
+          </div>
+        </div>
       </div>
     )
   }
