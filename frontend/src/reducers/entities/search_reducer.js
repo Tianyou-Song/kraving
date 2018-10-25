446d8d9eeaa3be7d4d409ec188e606e5c038dcfa
Tianyou-Song <tim.d.song@gmail.com>
2018-10-25 14:53:01 -0700

    fix business state

--- frontend/src/reducers/entities/search_reducer.js
+++ frontend/src/reducers/entities/search_reducer.js
@@ -3,13 +3,13 @@ import {
   GET_BIZ
 } from '../../util/yelp_api_util';
 
-const searchReducer = (state = [], action) => {
+const searchReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case SET_SEARCH_RESULTS:
       return action.payload;
     case GET_BIZ:
-      return action.business;
+      return  {...state, ...{[action.business.id]: action.business}};
     default:
       return state;
   }
