7a02b86f20d26b0eee934c62e8d4af4054ee2774
Stephen Li <stephen.li305@gmail.com>
2018-10-26 14:39:00 -0700

    create review folder

---
+++ frontend/src/components/review/review.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './review.css'
+// import HeaderContainer from '../header/header_container';
