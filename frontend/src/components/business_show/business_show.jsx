35fe2464ed63a2ef404404fc6c0bb2090818b25c
Stephen Li <stephen.li305@gmail.com>
2018-10-27 15:14:35 -0700

    add yelp and zomato review

--- frontend/src/components/business_show/business_show.jsx
+++ frontend/src/components/business_show/business_show.jsx
@@ -5,7 +5,6 @@ import './business_show.css';
 import { yelpBiz, yelpReviews, clearReviews } from '../../util/yelp_api_util.js';
 import { getZomatoReviews } from '../../util/zomato_api_util.js';
 import HeaderContainer from '../header/header_container';
-import './business_show.css'
 import './review.css'
 
 const mapStateToProps = (state, ownProps) => {
@@ -108,35 +107,33 @@ class BusinessShow extends React.Component {
 
 
   reviewGrid(){
-    const SEED_REVIEW = [
-    {
-      id: 1,
-      user_image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/uQ3_iuhf4H8asAuNw0woYg/o.jpg",
-      user_name:"Tim Song",
-      text: "Taqueria Guadalajara is pretty good, with some great meat choices. My favorites so far have definitely been the grilled chicken and carnitas. The carne asada was also good, but just didn't stand out relative to all the other Mexican restaurants. The grilled chicken had a great flavor and a great grilled texture on the outside. The carnitas were also a bit crispy, and not just soft and soggy, which I prefer. Having crispy carnitas can often leave them tasting dry, but that wasn't the case at all here. The burritos are huge! The prices are a bit higher than what Im used to for a burrito, but the size and amount of meat comes close to justifying the price. I also liked the use of avocado slices in the burritos, as opposed to guacamole. The service is very quick and it doesnt take long to get your food, even when busy. The horchata here is also good, although a bit sweeter than Id personally like, but most of them are.I will say I was a bit disappointed with the salsa bar. Im not sure if it was just a fluke when I went, but there were really only three options when I went: red (tomato), green (tomatillo) and habanero. The red and green salsas were not spicy at all, but the habanero salsa was quite spicy, and I have a very high tolerance. I would have liked to see more variety of salsas.",
-      rating: 4
-    },
-    {
-      id: 2,
-      user_image_url: "https://s3-media1.fl.yelpcdn.com/bphoto/uQ3_iuhf4H8asAuNw0woYg/o.jpg",
-      user_name:"Tim Song",
-      text: "Taqueria Guadalajara is pretty good, with some great meat choices. My favorites so far have definitely been the grilled chicken and carnitas. The carne asada was also good, but just didn't stand out relative to all the other Mexican restaurants. The grilled chicken had a great flavor and a great grilled texture on the outside. The carnitas were also a bit crispy, and not just soft and soggy, which I prefer. Having crispy carnitas can often leave them tasting dry, but that wasn't the case at all here. The burritos are huge! The prices are a bit higher than what Im used to for a burrito, but the size and amount of meat comes close to justifying the price. I also liked the use of avocado slices in the burritos, as opposed to guacamole. The service is very quick and it doesnt take long to get your food, even when busy. The horchata here is also good, although a bit sweeter than Id personally like, but most of them are.I will say I was a bit disappointed with the salsa bar. Im not sure if it was just a fluke when I went, but there were really only three options when I went: red (tomato), green (tomatillo) and habanero. The red and green salsas were not spicy at all, but the habanero salsa was quite spicy, and I have a very high tolerance. I would have liked to see more variety of salsas.",
-      rating: 4
-    }
-  ]
-
     // map over each business
-    const mapReviews =
-      SEED_REVIEW.map( review => {
+    const mapYelp =
+      this.props.reviews.yelpReviews.map( review => {
         // debugger
 
             return(
               <div className="review-card-container" key={review.id}>
-                <div className="review-card-image"><img src={review.user_image_url}/></div>
+                <div className="review-card-image"><img src={review.user.image_url}/></div>
                 <div className="review-card-detail">
-                  <div className="review-card-name">{review.user_name}</div>
+                  <div className="review-card-name">{review.user.name}</div>
                   <div className="review-card-rating"><img src={`/images/stars/${review.rating}.png`}></img></div>
-                  <div className="review-card-text">{review.text}</div>
+                  <div className="review-card-text"><a href={review.url}>{review.text}</a></div>
+                </div>
+              </div>
+            )
+          })
+
+    const mapZomato =
+      this.props.reviews.zomatoReviews.user_reviews.map( review => {
+
+            return(
+              <div className="review-card-container" key={review.review.id}>
+                <div className="review-card-image"><img src={review.review.user.profile_image}/></div>
+                <div className="review-card-detail">
+                  <div className="review-card-name">{review.review.user.name}</div>
+                  <div className="review-card-rating"><img src={`/images/stars/${review.review.rating}.png`}></img></div>
+                  <div className="review-card-text"><a href={review.review.user.profile_url}>{review.review.review_text}</a></div>
                 </div>
               </div>
             )
@@ -144,7 +141,8 @@ class BusinessShow extends React.Component {
 
     return(
       <div className="food-list-container-row">
-        {mapReviews}
+        {mapYelp}
+        {mapZomato}
       </div>
     )
   }
@@ -152,14 +150,19 @@ class BusinessShow extends React.Component {
 
   render(){
     const seedCity = "San Francisco"
-    return(
-      <div className="business-show-page-container">
+    if (Object.keys(this.props.reviews.yelpReviews).length <= 0 ||
+    Object.keys(this.props.reviews.zomatoReviews).length <= 0) {
+      return null}
+      return(
+        <div className="business-show-page-container">
         {this.businessHeader()}
         <h1 className="business-show-index-title">Most Popular Reviews</h1>
         {this.reviewGrid()}
         <link href="css/hover.css" rel="stylesheet" media="all"/>
-      </div>
-    )
+        </div>
+      )
+
+
   }
 }
 
