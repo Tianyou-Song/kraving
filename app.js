4e766427169acb90f1b24cf1a666da61ca428111
Tyler <tylerthomasallen@gmail.com>
2018-10-28 11:48:03 -0700

    Add changes for heroku push

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const yelp = require('./routes/api/yelp');
 const google = require('./routes/api/google');
 const zomato = require('./routes/api/zomato');
 const passport = require('passport');
+const path = require('path');
 require('./config/passport')(passport);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 mongoose
